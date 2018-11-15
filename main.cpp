893fe967c11e199258a88d3ae6dbce2cf15bdf4c
Neil Tan <neil.c.tan@gmail.com>
2018-11-15 14:50:58 +0800

    getting ready to debug:hard fault

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
-#include "models/deep_mlp.hpp"
-#include "SDBlockDevice.h"
-#include "tensorIdxImporter.hpp"
+#include "models/deep_mlp1.hpp"
 #include "tensor.hpp"
-#include "FATFileSystem.h"
 #include "mbed.h"
 
 Serial pc(USBTX, USBRX, 115200);
-SDBlockDevice bd(MBED_CONF_APP_SD_MOSI, MBED_CONF_APP_SD_MISO,
-                 MBED_CONF_APP_SD_CLK, MBED_CONF_APP_SD_CS);
-FATFileSystem fs("fs");
 
 void run_mlp(){
-  TensorIdxImporter t_import;
-  Tensor* input_x = t_import.float_import("/fs/tmp.idx");
+  Tensor* input_x = new RamTensor<float>({1, 784});
+  float* in_buff = input_x->write<float>(0, 1);
+  
+  for(int i = 0; i < 784; i++) {
+    in_buff[0] = (float) i;
+  }
+
   Context ctx;
 
-  get_deep_mlp_ctx(ctx, input_x);
+  get_deep_mlp1_ctx(ctx, input_x);
   S_TENSOR pred_tensor = ctx.get("y_pred:0");
   ctx.eval();
 
@@ -25,15 +24,9 @@ void run_mlp(){
 }
 
 int main(void) {
-  printf("Simple MNIST end-to-end uTensor cli example (device)\n");
-  
-  ON_ERR(bd.init(), "SDBlockDevice init ");
-  ON_ERR(fs.mount(&bd), "Mounting the filesystem on \"/fs\". ");
+  printf("Simple MNIST end-to-end CMSIS-NN uTensor cli playground (device)\n");
 
   run_mlp();
-  
-  ON_ERR(fs.unmount(), "fs unmount ");
-  ON_ERR(bd.deinit(), "SDBlockDevice de-init ");
 
   return 0;
 }
